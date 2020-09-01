878927555f5fe48d902ea6a9e740b8770fa55440
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-09-01 12:28:33 -0400

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 - Save the {SHA-512|SHA-256|SHA-1|MD5} with the string in a json file.
 */
 
+func GenerateString() {
+	// Generate a random string
+}
+
+func GetHash() {
+	// Get the Hash
+}
+
 func main() {
+	// Save to the file
 	fmt.Println("Hello, World!")
 }
