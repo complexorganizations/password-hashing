b320ee8bd2802175a5230a99e177f9a3bd5fb5ea
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-09-01 12:26:28 -0400

    Update main.go

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ import (
 - Save the {SHA-512|SHA-256|SHA-1|MD5} with the string in a json file.
 */
 
-/*
-{"random_string_0":{"sha-1":"sha-1_string_here","sha-256":"sha-256_string_here","sha-512":"sha-512_string_here","md5":"md5_string_here"},"random_string_1":{"sha-1":"sha-1_string_here","sha-256":"sha-256_string_here","sha-512":"sha-512_string_here","md5":"md5_string_here"}}
-*/
-
 func main() {
 	fmt.Println("Hello, World!")
 }
